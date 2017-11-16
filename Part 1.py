1609d7425a57f902140bc5f5079f55b927145391
NicolasVandeput <33733457+NicolasVandeput@users.noreply.github.com>
2017-11-16 21:34:51 +0100

    Delete Part 1.py

--- Part 1.py
+++
@@ -1,8 +0,0 @@
-# - Import module from sklearn
-from sklearn.tree import DecisionTreeRegressor
- 
-# - Instantiate a Decision Tree Regressor
-tree = DecisionTreeRegressor(max_depth=5,max_features=11,min_samples_leaf=5)
- 
-# - Fit the tree to the training data
-tree.fit(X_train,Y_train)
